eb998f5832be773a1d0d13dad305c38be61de2d1
mauro paredes <m.paredes943@gmail.com>
2021-08-06 02:37:56 -0300

    Corrigiendo cositas para el final

--- src/Pages/Contacto/Contacto.js
+++ src/Pages/Contacto/Contacto.js
@@ -17,7 +17,7 @@ function Contacto() {
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1380.762103114836!2d-58.381623204213774!3d-34.60354869487722!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4aa9f0a6da5edb%3A0x11bead4e234e558b!2sObelisco!5e0!3m2!1ses-419!2sar!4v1628216184009!5m2!1ses-419!2sar"
           style={{
             width: 600,
-            height: 450,
+            height: 400,
             border: 0,
             allowfullscreen: 0,
             loading: "lazy",
